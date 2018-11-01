d79a004bdfd17ddefaf08064e02cb98ca7e99930
mparedes003 <michelle.g.paredes@gmail.com>
2018-11-01 12:22:04 -0700

    Debugging

--- config/notesRoutes.js
+++ config/notesRoutes.js
@@ -3,10 +3,11 @@ const express = require('express');
 
 const knex = require('knex');
 
-const dbEngine = process.env.DB || 'development';
-const knexConfig = require('../knexfile.js')[dbEngine];
+// const dbEngine = process.env.DB || 'development';
+const knexConfig = require('../knexfile.js');
+// [dbEngine];
 
-const db = knex(knexConfig);
+const db = knex(knexConfig.development);
 
 const router = express.Router();
 
