3a64131d8e0bf29cc43023e4c8e294083c90c228
mparedes003 <onnabugeishadev@gmail.com>
2019-04-30 15:57:58 -0700

    Transfer dbconfig to its own file

---
+++ data/dbconfig.js
@@ -0,0 +1,7 @@
+const knex = require("knex");
+require("dotenv").config();
+
+const dbEngine = process.env.DB || "development";
+const knexConfig = require("../knexfile.js")[dbEngine];
+
+module.exports = knex(knexConfig);
